4e62f70f4a1ab076c8ad552ab329ce6b88afd5ee
Caio Marcelo Cavallari Ruas <caio24010@ilum.cnpem.br>
2026-04-29 18:04:22 -0300

    Atualizações para GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://CaioRuas24010.github.io'
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentacao-caca-asteroides/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'CaioRuas24010',
+  projectName: 'documentacao-caca-asteroides',
 
   onBrokenLinks: 'throw',
 
